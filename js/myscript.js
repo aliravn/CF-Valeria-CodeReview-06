b78ab6ea0319bf2e82d12f71ecc13f6710f5d2bf
Valeria Nazarova <alira.vn@gmail.com>
2019-08-09 12:29:39 +0200

    add function to create a new div with rext as input value

--- js/myscript.js
+++ js/myscript.js
@@ -1,9 +1,13 @@
 const addTaskButton = $("#addTask");
 const input = $("#input");
+const taskList = $("#tasks");
 
 addTaskButton.click(addTask);
 function addTask(){
 	console.log("hello from addTaskButton");
 	console.log(input.val());
-
+	var taskText = document.createTextNode(input.val());
+	var div = document.createElement("div")
+	div.append(taskText);  
+	taskList.append(div);
 }
