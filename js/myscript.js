d7c8d410e9354c07d008fe957084c6067d9edf5f
Valeria Nazarova <alira.vn@gmail.com>
2019-08-09 12:12:13 +0200

    create function on addTaskButton to get value of input field

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,9 @@
-console.log("hello");
+const addTaskButton = $("#addTask");
+const input = $("#input");
+
+addTaskButton.click(addTask);
+function addTask(){
+	console.log("hello from addTaskButton");
+	console.log(input.val());
+
+}
