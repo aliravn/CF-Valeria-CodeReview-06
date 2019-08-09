20bb9c14568ac3ecd245915b1d2f2bb8da17b269
Valeria Nazarova <alira.vn@gmail.com>
2019-08-09 17:14:45 +0200

    add counter to display all and open tasks

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,8 @@
 const addTaskButton = $("#addTask");
 const taskList = $("#tasks");
 const input = $("#input");
+var counterAll = 0;
+var counterOpen = counterAll;
 
 addTaskButton.click(addTask);
 
@@ -12,25 +14,34 @@ function addTask(){
 			<button class="doneButton" id="done${id}"><i class="fas fa-check-square"></i></button>
 			<button class="removeButton" id="rmv${id}"><i class="fas fa-times-circle"></i></button>
 		</div>`);
-	console.log(id);
 	$(`#rmv${id}`).click(removeTask);
-	$(`#done${id}`).click(markDone).dblclick(markNotDone);
+	$(`#done${id}`).click(markDone).dblclick(markOpen);
 	$(`#task${id}`).addClass("open");
 	id +=1;
-	console.log(id);
+	displayCounter();
 }
 
 function removeTask() {
 	console.log("helo from removeTaskbutton");
 	$(this).closest('.list').remove();
+	displayCounter();
 }
 
 function markDone() {
 	console.log("helo from markDoneButton");
 	$(this).closest('.list').removeClass("open").addClass("done");
+	displayCounter();
 }
 
-function markNotDone() {
-	console.log("helo from markDoneButton");
+function markOpen() {
+	console.log("helo from markOpen");
 	$(this).closest('.list').removeClass("done").addClass("open");
+	displayCounter();
+}
+
+function displayCounter() {
+	counterAll = $(".list").length;
+	counterOpen = $(".open").length;
+	$("#counterAll").text(counterAll);
+	$("#counterOpen").text(counterOpen);
 }
